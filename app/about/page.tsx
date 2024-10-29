5d26c0a39f9c051d9febb8533dce00780a163bf2
lokesh-22 <lokeshboddupally13@gmail.com>
2024-10-29 12:58:30 +0530

    Contact us page is done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,11 +6,21 @@ export default function AboutUsPage() {
     return(
         <div>
         <Navbar/>
-        <div className='flex justify-center items-center min-h-screen'>
-            <h1>About us</h1>
-            </div>
-            <Footer/>
-    </div>
+
+        <div className="flex justify-center items-center min-h-screen">
+        <div className="bg-gray-100 border border-gray-300 p-8 rounded-lg shadow-lg text-center w-2000 h-48">
+  <h2 className="text-2xl font-bold mb-4">Introduction</h2>
+  <p className="text-gray-700">Welcome to our platform! Here you’ll find everything you need to know about us.
+  </p>
+</div>
+
+</div>
+
+       
+
+           
+        <Footer/>
+        </div>
     
     );
 
