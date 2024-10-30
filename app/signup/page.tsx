4253cb285ae7383a03c25dbc819f81de9cdce783
Praneeth Palugula <praneeth.palugula@gmail.com>
2024-10-30 19:20:38 +0530

    minor bug fix

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,7 +5,7 @@ import banner from "@/assets/banner2.jpg";
 import Footer from "@/components/Footer";
 import axios from "axios";
 import { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function SignupPage() {
     const [formData, setFormData] = useState({
